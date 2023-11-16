6bbd8945f336a865eec47c234de029fcb7967ee2
Natalia Macias <natajenkins@hotmail.com>
2023-11-15 21:14:14 -0600

    main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import "./App.scss";
 function App() {
-    return <>hola</>;
+    return (
+        <main>
+            <div className="container">
+                <div className="container-content">
+                    <div className="header">
+                        <h1 className="title">
+                            Reliable, efficient delivery <br />
+                            <span>Powered by Technology</span>
+                        </h1>
+                        <p className="sub-title">
+                            Our Artificial Intelligence powered tools use
+                            millions of project data points to ensure that your
+                            project is successful
+                        </p>
+                    </div>
+                    <div className="cards-container">hola</div>
+                </div>
+            </div>
+        </main>
+    );
 }
 
 export default App;
